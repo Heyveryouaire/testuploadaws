5a91e14f3edeb76fdcc70e1bad08ed01c0437257
Heyveryouaire <mathieu.pavy.29@gmail.com>
2021-07-02 15:27:23 +0200

    fix url

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ async function sendData(data) {
     console.log("OBJ", obj)
 
     let XHR = new XMLHttpRequest();
-    XHR.open('POST', 'https://	https://279htbz50g.execute-api.us-east-1.amazonaws.com/image');
+    XHR.open('POST', 'https://279htbz50g.execute-api.us-east-1.amazonaws.com/image');
     //XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     XHR.send(obj);
 }
